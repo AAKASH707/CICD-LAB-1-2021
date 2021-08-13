b97db77087ad8dc82cb418438865008a9c142101
AAKASH707 <lucifer6218@gmail.com>
2021-08-14 04:46:46 +0530

    Second commit !

--- src/NewClass.java
+++ src/NewClass.java
@@ -3,6 +3,7 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Output print line for out put");
+		System.out.println("Second print line for project");
 
 	}
 
