25fe4bab1fdcfa0d40227280538d395290c5953c
AAKASH707 <lucifer6218@gmail.com>
2021-08-14 04:51:30 +0530

    Last commit before auto build !

--- src/NewClass.java
+++ src/NewClass.java
@@ -4,6 +4,8 @@ public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("Output print line for out put");
 		System.out.println("Second print line for project");
+		
+		System.out.println("Third line");
 
 	}
 
